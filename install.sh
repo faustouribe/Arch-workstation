e8dfe9e072633d68f9ad17354cf7440d783c5efb
faustouribe <faustouribe@gmail.com>
2022-08-28 15:17:43 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Arch-Workstation Install Script
+# Scripted by FUF Aug 28 2022
+
+### OPTIONS AND VARIABLES ###
+programsfile="https://raw.githubusercontent.com/faustouribe/arch-workstation/main/programs.csv"
+aurhelper="yay"
